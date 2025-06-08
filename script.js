cfdfa62aebe09b758c607b1b542691530a798145
Matheus1237 <masian202207@gmail.com>
2025-06-08 11:13:05 -0300

    git commit -m "first commit"

---
+++ script.js
@@ -0,0 +1,5 @@
+// JS básico de exemplo
+window.addEventListener('DOMContentLoaded', function() {
+    // Alerta de boas-vindas (remova se não quiser)
+    // alert('Bem-vindo ao meu portfólio!');
+}); 
